e575ad627729f1f32793ce01c3e33a9a7a461012
PhamTatThanh-VNU <thanhpham2k4nz@gmail.com>
2024-11-23 11:41:34 +0700

    change something

--- src/main/java/com/example/qairlines/Controller/LocationController.java
+++ src/main/java/com/example/qairlines/Controller/LocationController.java
@@ -5,6 +5,7 @@ import com.example.qairlines.Services.LocationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/locations")
 @RequiredArgsConstructor
+@CrossOrigin
 public class LocationController {
     private final LocationService locationService;
 
